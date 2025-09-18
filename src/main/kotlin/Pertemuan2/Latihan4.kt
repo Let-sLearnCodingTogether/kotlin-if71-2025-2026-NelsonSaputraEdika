ff8b3136ca1bf692c59085cbab7d5e5c9583818e
nelson <145772816+NelsonSaputraEdika@users.noreply.github.com>
2025-09-18 20:07:17 +0700

    Pertemuan 3 lanjutan

---
+++ src/main/kotlin/Pertemuan2/Latihan4.kt
@@ -0,0 +1,12 @@
+package Pertemuan2
+
+fun main() {
+    val namaAwal : String = "Nelson"
+    val namaAkhir : String = "Saputra Edika"
+    var score : Int = 0
+
+    score = 1
+
+
+    print("Nama Lengkap : $namaAwal $namaAkhir , Score : $score")
+}
---
+++ src/main/kotlin/pertemuan1/String.kt
@@ -0,0 +1,12 @@
+package pertemuan1
+
+fun main() {
+    val university : String = "Universitas Multi Data Palembang"
+    val address : String = """
+        Jln.Rajawali
+        Sumatera Selatan
+        Palembang
+    """.trimMargin()
+
+    println(address)
+}
