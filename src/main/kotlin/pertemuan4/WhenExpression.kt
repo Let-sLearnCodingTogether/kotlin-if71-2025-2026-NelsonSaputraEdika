4c4ce1efeb87a990e75ddb8752e030e0f271a0b1
Rvlnaaa <wewenrevalona_2428250066@mhs.mdp.ac.id>
2025-09-19 19:47:03 +0700

    lanjutan

---
+++ src/main/kotlin/pertemuan4/WhenExpression.kt
@@ -0,0 +1,29 @@
+package pertemuan4
+
+fun main () {
+    val finalExam = 'A'
+
+    when(finalExam) {
+        'A' -> println("Lulus")
+        'B' -> println("Lulus juga")
+        'C' -> println("Ya bisa lulus")
+        else -> println("Tidak lulus")
+    }
+    when(finalExam) {
+        'A','B' -> println("Lulus")
+        'C' -> println("Ya bisa lulus")
+        else -> println("Tidak lulus")
+    }
+
+    when {
+        finalExam == 'A' || finalExam == 'B' -> println("lulus")
+        finalExam == 'C' -> println("Ya Bisa lulus")
+        else -> println("Tidak Lulus")
+    }
+    var tesKriuk = 5
+
+    when  {
+        tesKriuk > 2 -> tesKriuk = 20
+    }
+    println(tesKriuk)
+}
